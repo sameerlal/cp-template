0254ccb6f5b5419aa1eed964aa7622271082cfe2
Sameer <sjl328@cornell.edu>
2020-06-24 16:13:07 -0400

    templated

--- Makefile
+++ Makefile
@@ -1,16 +1,21 @@
-A: p1.cpp
+a: p1.cpp
 	g++ p1.cpp -std=c++14 -O2
 t: ./a.out
 	./a.out
-B: p2.cpp
+b: p2.cpp
 	g++ p2.cpp -std=c++14 -O2
-C: p3.cpp
+c: p3.cpp
 	g++ p3.cpp -std=c++14 -O2
-D: p4.cpp
+d: p4.cpp
 	g++ p4.cpp -std=c++14 -O2
-E: p5.cpp
+e: p5.cpp
 	g++ p5.cpp -std=c++14 -O2
 all:
 	make A; make B; make C; make D; make E;
 clean:
-	rm -rf *vscode .DS* *swp
+	rm -rf *vscode .DS* *swp;
+	cp template.cpp p1.cpp;
+	cp template.cpp p2.cpp;
+	cp template.cpp p3.cpp;
+	cp template.cpp p4.cpp;
+	cp template.cpp p5.cpp;
---
+++ p5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <string.h>
+#include <unordered_map>
+#include <bits/stdc++.h>
+#include <set>
+#include <vector>
+#include <bitset>
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define fill(x, y) memset(x, y, sizeof(y))
+#define debug(x)                          \
+    {                                     \
+        cerr << #x << " = " << x << endl; \
+    }
+typedef long long ll;
+const int MOD = 1e9 + 7;
+using namespace std;
+ 
+void solve()
+{
+    int n; cin >> n;
+    ll arr[n];
+    for (int i = 0;i<n;i++)
+        cin >> arr[i];
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // int t; t = 1; 
+    int t; cin >> t;
+    while (t--)
+        solve();
+}
